92bfeb2c788ee9668aadb5fcecc9802b9982ad82
Aleeya Dasgupta <aleeya@gmail.com>
2018-03-30 13:07:10 +0530

    edited view and controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
 
-	before_action :find_article, only: [:show, :edit, :update, :destroy]
+	before_action :set_article, only: [:show, :edit, :update, :destroy]
 
 	def index
 		@articles = Article.all
@@ -13,30 +13,35 @@ class ArticlesController < ApplicationController
 		@article = Article.new
 	end
 
+	def edit
+	end
+
 	def create
 		@article = Article.new(article_params)
-		@article.save
-		redirect_to @article
+		if @article.save
+			redirect_to @article, notice: 'Article was added'
+		else
+			render :new
+		end
 	end
 
 	def update
-		@article.update(article_params)
-		@article.save
-		redirect_to @article
-	end
-
-	def edit
+		if @article.update(article_params)
+			redirect_to @article, notice: 'Article was updated'
+		else
+			render :edit
+		end
 	end
 
 	def destroy
 		@article.destroy
-		redirect_to articles_path
+		redirect_to articles_url, notice: 'Article was deleted'
 	end
 
 	private
 
-	def find_article
-		@article = Article.find(params[:id])
+	def set_article
+		@article = Article.find_by(params[:id])
 	end
 
 
